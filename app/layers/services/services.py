661dd8f465e95e3866675b13e6367cee764414fb
unknown <santi.peralta99@gmail.com>
2024-11-20 17:01:47 -0300

    Corregido punto 2, implementado login, implementada busqueda

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -7,17 +7,18 @@ from ..transport import transport
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = transport.getAllImages()
+    json_collection = transport.getAllImages(input)
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
-    images = [translator.fromRequestIntoCard(image) for image in json_collection]
-
+    images = []
+    for image in json_collection:
+        images.append(translator.fromRequestIntoCard(image))
     return images
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
-    fav = '' # transformamos un request del template en una Card.
-    fav.user = '' # le asignamos el usuario correspondiente.
+    fav = translator.fromRequestIntoCard(request) # transformamos un request del template en una Card.
+    fav.user = get_user(request) # le asignamos el usuario correspondiente.
 
     return repositories.saveFavourite(fav) # lo guardamos en la base.
 
@@ -28,11 +29,11 @@ def getAllFavourites(request):
     else:
         user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
+        favourite_list = repositories.getAllFavourites(user) # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
+            card = translator.fromRepositoryIntoCard(favourite) # transformamos cada favorito en una Card, y lo almacenamos en card.
             mapped_favourites.append(card)
 
         return mapped_favourites
--- app/views.py
+++ app/views.py
@@ -8,39 +8,44 @@ from django.contrib.auth import logout
 def index_page(request):
     return render(request, 'index.html')
 
+
+def getAllImagesAndFavouriteList(search_msg, request):
+    images = services.getAllImages(search_msg)
+    favourite_list = services.getAllFavourites(request)
+
+    return images, favourite_list
+
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
     images = services.getAllImages()
     favourite_list = []
-
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 def search(request):
     search_msg = request.POST.get('query', '')
-
-    # si el texto ingresado no es vacío, trae las imágenes y favoritos desde services.py,
-    # y luego renderiza el template (similar a home).
-    if (search_msg != ''):
-        pass
-    else:
-        return redirect('home')
-
+    if search_msg == "":
+        search_msg = None
+    images, favourite_list = getAllImagesAndFavouriteList(search_msg, request)
+    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list})
 
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = []
+    favourite_list = services.getAllFavouritesByUser(request)
     return render(request, 'favourites.html', { 'favourite_list': favourite_list })
 
 @login_required
 def saveFavourite(request):
-    pass
+    services.saveFavourite(request)
+    return redirect("/buscar")
 
 @login_required
 def deleteFavourite(request):
-    pass
+    services.deleteFavourite
+    return redirect("/favourites")
 
 @login_required
 def exit(request):
-    pass
+    logout(request)
+    return redirect('/')
